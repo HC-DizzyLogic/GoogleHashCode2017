22fb3f6bfee0b516ae02d878be40a69ff04ee446
Pelayo Choya <pelayo.choya@gmail.com>
2017-02-23 19:55:26 +0100

    Update HASH.cpp

--- HASH.cpp
+++ HASH.cpp
@@ -1,50 +1,26 @@
-#include "Endpoint.h"
+#include "hash.h"
+#include "cache.h"
+#include "endpoint.h"
+
+
+using namespace std;
 
 int main() {
-	data_center center;
-	vector<Cache> caches;
-	vector<Endpoint> endpoints;
+    data_center center;
+    vector<Cache> caches;
+    vector<Endpoint> endpoints;
 
 
-	ofstream myfile;
-	myfile.open ("result.txt");
-	myfile << "\n";
-	myfile.close();
-	return 0;
+    ofstream myfile;
+    myfile.open ("result.txt");
+    myfile << "\n";
+    myfile.close();
+    return 0;
 }
 
-void leeFichero(string name, data_center & center, vector<Cache> & caches, vector<Endpoint> & endpoints){
-		string linea;
-		ifstream infile;
-		infile.open (name);
-		string firstline="";
-
-		getline(infile,firstline);
-
-		stringstream ss;
-		ss.str("");
-		ss.clear();
-		ss << firstline;
-
-		ss >> rows;
-		ss >> cols;
-		ss >> min;
-		ss >> max;
-
-		cout << "nº de filas: " << rows << ", nº de columnas: " << cols << endl;
-		cout << "nº de ingredientes de cada tipo: " << min << " , nº max de casillas: " << max << endl;
-
-		pizza = vector<vector<char> > (rows, vector<char> (cols, 0));
-
-		for (int i = 0; i < rows; i++){ // To get you all the lines.
-			getline(infile,linea); // Saves the line in STRING.
-			for (int j = 0; j < cols; j++){
-				pizza[i][j] = linea.at(j);
-				cout << pizza[i][j];
-			}
-			cout<< endl;
-		}
-	}
+void leeFichero(string name, data_center & center, vector<Cache> & caches, vector<Endpoint> & endpoints)
+{
+      }
  void brute_lattency_force ()
 {
     //This function will provide the brute lattency of each video
