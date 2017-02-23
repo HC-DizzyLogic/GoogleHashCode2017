326675c11b611a51c95452ec040d95f9669bdce2
Pelayo Choya <pelayo.choya@gmail.com>
2017-02-23 19:40:52 +0100

    function brute lattency

--- HASH.cpp
+++ HASH.cpp
@@ -45,3 +45,20 @@ void leeFichero(string name, data_center & center, vector<Cache> & caches, vecto
 			cout<< endl;
 		}
 	}
+    //This function will provide the brute lattency of each video
+    vector<int> nodo = {1,2,3};
+    int no_times = 1500;
+    vector<int> brute_lattency;
+    int lattency = 0;
+    int delay_datacenter = 1000;
+    int no_requests = 3;
+        for ( int i = 0; i < nodo.size(); i++ )
+            {
+                cout << "huehue" << endl;
+                lattency = delay_datacenter * no_requests;
+                brute_lattency.push_back(lattency);
+            }
+        for ( int i = 0; i < brute_lattency.size(); i++)
+            {
+                cout << brute_lattency[i] << endl;
+            }
